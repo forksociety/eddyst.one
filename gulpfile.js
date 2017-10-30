e268a623a3cf7ed30360118d8ad4f432949a5e54
Christof Lee <christoflee63@gmail.com>
2017-10-30 10:10:25 +0000

    Feat: Setup sass task
    - Move our build and generate our dist files
    - Install NPM package

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp   = require("gulp");
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
+var sass   = require("gulp-sass");
 
 var config = {
     path: {
@@ -13,5 +14,12 @@ gulp.task( 'js', function() {
     gulp.src( config.path.build + '/js/*.js' )
         .pipe( uglify() )
         .pipe(concat('script.js'))
+
+// Minify our SCSS files
+gulp.task( "sass", function() {
+    gulp.src( config.path.build + "/scss/*.scss" )
+        .pipe( sass({
+            outputStyle: "compressed"
+        }) )
         .pipe( gulp.dest( config.path.dist ) )
 });
