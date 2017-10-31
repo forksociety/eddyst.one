555b635216ffadd36a3683fa778364e3d57c3bcf
Christof Lee <christoflee63@gmail.com>
2017-10-31 21:19:57 +0000

    Feat: Setup CSS and JS tasks to handle partials
    - Replace old CSS minified file with SCSS
    - Replace old form JS file ready to add single JS script
    - Add gulp-concat
    - Add gulp-sass
    - Add vendor path
    - Add JS task to concat and uglify our JS
    - Uninstall gulp-rename (not in used)
    - Add task to utilise sass and minify our CSS
    - Watch our files for changes
    - Amend default task
    - Amend URLs to files in index to single CSS file and single JS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,48 @@
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
-var rename = require("gulp-rename");
+var concat = require("gulp-concat");
 var cleanCSS = require("gulp-clean-css");
+var sass = require("gulp-sass");
 
 var config = {};
 config.srcPath = "./src/";
 config.distPath = "./dist/";
+config.vendorPath = "./vendor/";
 
-config.cssSrcPath = config.srcPath + "css/*.css";
+config.sassSrcPath = config.srcPath + "sass/*.scss";
 config.jsSrcPath = config.srcPath + "js/*.js";
 
-config.cssDistPath = config.distPath + "css/";
-config.jsDistPath = config.distPath + "js/";
+// JS files to concatenate
+config.jsFilesToConcat = [
+    // The order of files here *matters*
+    config.vendorPath + "form.js"
+];
 
-// CSS Minification
-gulp.task("css", () => {
-  return gulp.src(config.cssSrcPath)
-    .pipe(cleanCSS({compatibility: "ie8", level: {1: {specialComments: 0}} }))
-    .pipe(rename({
-    	suffix: ".min"
-    }))
-    .pipe(gulp.dest(config.cssDistPath));
+// Minify our SCSS files
+gulp.task("sass", () => {
+    return gulp.src(config.sassSrcPath)
+        .pipe(cleanCSS({
+            compatibility: "ie8", level: {1: {specialComments: 0}}
+        }))
+        .pipe(sass({
+            outputStyle: "compressed"
+        }))
+        .pipe(gulp.dest(config.distPath));
 });
 
-// JS Minification
+// Join and uglify our JS files
 gulp.task("js", () => {
-  return gulp.src(config.jsSrcPath)
-    .pipe(uglify())
-    .pipe(rename({
-    	suffix: ".min"
-    }))
-    .pipe(gulp.dest(config.jsDistPath));
+    return gulp.src(config.jsFilesToConcat)
+        .pipe(concat("script.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.distPath));
 });
 
-// gulp watch for css and js
-gulp.task("watch", () => {
+// Watch files for changes
+gulp.task("watch", ["sass", "js"], () => {
     gulp.watch(config.jsSrcPath, ["js"]);
-    gulp.watch(config.cssSrcPath, ["css"]);
+    gulp.watch(config.sassSrcPath, ["sass"]);
 });
 
-//gulp default
-gulp.task("default",  ["css", "js"]);
+// Gulp default
+gulp.task("default", ["sass", "js"]);
